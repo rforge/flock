2a58ed4f855b839f3fbf16483e0b96f976985f61
popivan <popivan@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-11-21 01:16:58 +0000

    Changes to the docs, the package builds without warnings now.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/flock@4 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/flock.r
+++ pkg/R/flock.r
@@ -12,6 +12,8 @@
 ## See the License for the specific language governing permissions and
 ## limitations under the License.
 
+require(methods)
+
 # The file locking interface
 lock = function(path, exclusive=TRUE)
 {
